7d663fb04902928f797beb0a61dd4e2b75aa3a94
Aryan Pingale <aryanpingale4@gmail.com>
2026-09-09 22:07:56 +0530

    Add single and multi-line comments in C program

---
+++ single_multiline.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+    //THis is a single-line comment
+    printf("Hello,World\n");
+    /* 
+    this is a multi-line comment
+    It can contain multiple lines 
+   */
+   return 0;
+}
